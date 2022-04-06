276df68f0870fcb294cb10ad72c89e34457c8af2
t-okada <t-okada@atware.co.jp>
2022-04-06 13:25:40 +0900

    レイアウト修正

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -18,24 +18,20 @@ export const App = () => {
     inputCommand(e.target.value);
   }, [inputCommand]);
 
-  const handleOnShortCut = useCallback((name: MoveName) => () => {
-    inputCommand(command + name);
-  }, [inputCommand]);
-
   return (
-    <div className="bg-black py-10 px-8 grid grid-cols-2">
-      <div>
+    <div className="bg-black h-full py-10 px-8 grid grid-cols-2">
+      <div className="mt-20 flex justify-center">
         <Cube cube={cube} moveArea={mouseOver ? getMoveArea(mouseOver) : undefined}/>
       </div>
       <div>
-        <div className="text-xl text-lime-500 font-semibold h-5/6 overflow-y-hidden">
-          {history.slice(-15).map(c => (<p className="tracking-[.15em]">{c}</p>))}
+        <div className="lg:text-xl text-lime-500 overflow-y-hidden lg:h-4/6 h-3/6">
+          {history.slice(-15).map(c => (<p className="tracking-widest">{c}</p>))}
           {progress !== 100 && <div className="bg-lime-500 h-4" style={{ width: `${progress}%` }}/>}
           {progress === 100 && (
             <>
-              <span className="text-xl text-lime-500 mr-4">{'>'}</span>
+              <span className="lg:text-xl text-lime-500 mr-4">{'>'}</span>
               <input
-                className="w-10/12 bg-black text-xl text-lime-500 caret-lime-500 focus:outline-0 tracking-[.15em]"
+                className="w-10/12 bg-black lg:text-xl text-lime-500 caret-lime-500 focus:outline-0 tracking-widest"
                 value={command}
                 onKeyPress={handleOnEnter}
                 onChange={handleOnChange}
@@ -46,14 +42,13 @@ export const App = () => {
           )}
         </div>
         <div>
-          <h3 className="lg:text-8xl md:text-2xl text-lime-500">{secondsToString(seconds)}</h3>
+          <h3 className="lg:text-8xl text-6xl text-lime-500">{secondsToString(seconds)}</h3>
         </div>
-        <div className="flex mt-3 space-x-3">
+        <div className="mt-3 grid xl:grid-cols-12 grid-cols-6">
           {MOVE_NAMES.map(name => (
             <button
               key={`shortcut-${name}`}
-              className="px-2 py-1 text-xl text-gray-100 w-10 bg-lime-500 text-white font-semibold rounded hover:bg-lime-400"
-              onClick={handleOnShortCut(name)}
+              className="px-2 py-1 text-xl text-gray-100 w-10 bg-lime-500 text-white font-semibold rounded hover:bg-lime-400 mb-3"
               onMouseOver={() => setMouseOver(name)}
               onMouseOut={() => setMouseOver(undefined)}
             >{name}</button>
--- src/components/cube/index.tsx
+++ src/components/cube/index.tsx
@@ -56,35 +56,35 @@ const getEOrientationOfReverse = (orientation: EdgeOrientation): EdgeOrientation
 
 export const Cube = ({ cube, moveArea }: { cube: CubeType, moveArea?: MoveArea }) => (
   <div>
-    <div className="ml-48">
-      <div className="w-48 flex">
+    <div className="lg:ml-36 ml-24">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[0]][getCOrientationOfRightSide(cube.cornerOrientation[0])]} isHover={moveArea?.corner[0]}/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[0]][cube.edgeOrientation[0]]} isHover={moveArea?.edge[0]}/>
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[4]][getCOrientationOfLeftSide(cube.cornerOrientation[4])]} isHover={moveArea?.corner[4]}/>
       </div>
-      <div className="w-48 flex">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[4]][getEOrientationOfReverse(cube.edgeOrientation[4])]} isHover={moveArea?.edge[4]}/>
         <CubeParts color="blue" label="B"/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[8]][getEOrientationOfReverse(cube.edgeOrientation[8])]} isHover={moveArea?.edge[8]}/>
       </div>
-      <div className="w-48 flex">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[1]][getCOrientationOfLeftSide(cube.cornerOrientation[1])]} isHover={moveArea?.corner[1]}/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[1]][cube.edgeOrientation[1]]} isHover={moveArea?.edge[1]}/>
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[5]][getCOrientationOfRightSide(cube.cornerOrientation[5])]} isHover={moveArea?.corner[5]}/>
       </div>
     </div>
-    <div className="ml-48">
-      <div className="w-48 flex">
+    <div className="lg:ml-36 ml-24">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[1]][getCOrientationOfRightSide(cube.cornerOrientation[1])]} isHover={moveArea?.corner[1]}/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[1]][getEOrientationOfReverse(cube.edgeOrientation[1])]} isHover={moveArea?.edge[1]}/>
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[5]][getCOrientationOfLeftSide(cube.cornerOrientation[5])]} isHover={moveArea?.corner[5]}/>
       </div>
-      <div className="w-48 flex">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[5]][getEOrientationOfReverse(cube.edgeOrientation[5])]} isHover={moveArea?.edge[5]}/>
         <CubeParts color="red" label="R"/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[9]][getEOrientationOfReverse(cube.edgeOrientation[9])]} isHover={moveArea?.edge[9]}/>
       </div>
-      <div className="w-48 flex">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[2]][getCOrientationOfLeftSide(cube.cornerOrientation[2])]} isHover={moveArea?.corner[2]}/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[2]][getEOrientationOfReverse(cube.edgeOrientation[2])]} isHover={moveArea?.edge[2]}/>
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[6]][getCOrientationOfRightSide(cube.cornerOrientation[6])]} isHover={moveArea?.corner[6]}/>
@@ -92,69 +92,69 @@ export const Cube = ({ cube, moveArea }: { cube: CubeType, moveArea?: MoveArea }
     </div>
     <div className="flex">
       <div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[1]][cube.cornerOrientation[1]]} isHover={moveArea?.corner[1]}/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[5]][cube.edgeOrientation[5]]} isHover={moveArea?.edge[5]}/>
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[2]][cube.cornerOrientation[2]]} isHover={moveArea?.corner[2]}/>
         </div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[4]][cube.edgeOrientation[4]]} isHover={moveArea?.edge[4]}/>
           <CubeParts color="white" label="W"/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[6]][cube.edgeOrientation[6]]} isHover={moveArea?.edge[6]}/>
         </div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[0]][cube.cornerOrientation[0]]} isHover={moveArea?.corner[0]}/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[7]][cube.edgeOrientation[7]]} isHover={moveArea?.edge[7]}/>
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[3]][cube.cornerOrientation[3]]} isHover={moveArea?.corner[3]}/>
         </div>
       </div>
       <div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[2]][getCOrientationOfRightSide(cube.cornerOrientation[2])]} isHover={moveArea?.corner[2]}/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[2]][cube.edgeOrientation[2]]} isHover={moveArea?.edge[2]}/>
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[6]][getCOrientationOfLeftSide(cube.cornerOrientation[6])]} isHover={moveArea?.corner[6]}/>
         </div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[6]][getEOrientationOfReverse(cube.edgeOrientation[6])]} isHover={moveArea?.edge[6]}/>
           <CubeParts color="green" label="G"/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[10]][getEOrientationOfReverse(cube.edgeOrientation[10])]} isHover={moveArea?.edge[10]}/>
         </div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[3]][getCOrientationOfLeftSide(cube.cornerOrientation[3])]} isHover={moveArea?.corner[3]}/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[3]][cube.edgeOrientation[3]]} isHover={moveArea?.edge[3]}/>
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[7]][getCOrientationOfRightSide(cube.cornerOrientation[7])]} isHover={moveArea?.corner[7]}/>
         </div>
       </div>
       <div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[6]][cube.cornerOrientation[6]]} isHover={moveArea?.corner[6]}/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[9]][cube.edgeOrientation[9]]} isHover={moveArea?.edge[9]}/>
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[5]][cube.cornerOrientation[5]]} isHover={moveArea?.corner[5]}/>
         </div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[10]][cube.edgeOrientation[10]]} isHover={moveArea?.edge[10]}/>
           <CubeParts color="yellow" label="Y"/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[8]][cube.edgeOrientation[8]]} isHover={moveArea?.edge[8]}/>
         </div>
-        <div className="w-48 flex">
+        <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[7]][cube.cornerOrientation[7]]} isHover={moveArea?.corner[7]}/>
           <CubeParts color={EDGE_PARTS[cube.edgePermutation[11]][cube.edgeOrientation[11]]} isHover={moveArea?.edge[11]}/>
           <CubeParts color={CORNER_PARTS[cube.cornerPermutation[4]][cube.cornerOrientation[4]]} isHover={moveArea?.corner[4]}/>
         </div>
       </div>
     </div>
-    <div className="ml-48">
-      <div className="w-48 flex">
+    <div className="lg:ml-36 ml-24">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[3]][getCOrientationOfRightSide(cube.cornerOrientation[3])]} isHover={moveArea?.corner[3]}/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[3]][getEOrientationOfReverse(cube.edgeOrientation[3])]} isHover={moveArea?.edge[3]}/>
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[7]][getCOrientationOfLeftSide(cube.cornerOrientation[7])]} isHover={moveArea?.corner[7]}/>
       </div>
-      <div className="w-48 flex">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[7]][getEOrientationOfReverse(cube.edgeOrientation[7])]} isHover={moveArea?.edge[7]}/>
         <CubeParts color="orange" label="O"/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[11]][getEOrientationOfReverse(cube.edgeOrientation[11])]} isHover={moveArea?.edge[11]}/>
       </div>
-      <div className="w-48 flex">
+      <div className="lg:w-36 lg:h-12 w-24 h-8 flex">
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[0]][getCOrientationOfLeftSide(cube.cornerOrientation[0])]} isHover={moveArea?.corner[0]}/>
         <CubeParts color={EDGE_PARTS[cube.edgePermutation[0]][getEOrientationOfReverse(cube.edgeOrientation[0])]} isHover={moveArea?.edge[0]}/>
         <CubeParts color={CORNER_PARTS[cube.cornerPermutation[4]][getCOrientationOfRightSide(cube.cornerOrientation[4])]} isHover={moveArea?.corner[4]}/>
